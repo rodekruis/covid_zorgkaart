cb9e55fcc7118683880884c542c6fa62db4571cc
aklilu <akliludin@gmail.com>
2020-03-30 15:44:40 +0200

    update the code to extract data per service provider types

--- covid.py
+++ covid.py
@@ -78,6 +78,60 @@ for items in output1:
         fname.write(';'.join([str(elem) for elem in items])+'\n') 
 fname.close()
   
+#%% for service provide
+# make sure the list of service providers has the name of all the services we need data for 
+service_list=["Verpleeghuis en verzorgingshuis",
+"Woonvoorziening voor lichamelijk gehandicapten",
+"Woonvoorziening voor verstandelijk gehandicapten",
+"Woonvoorziening voor visueel gehandicapten",
+"Instelling voor auditief gehandicapten",
+"Instelling voor lichamelijk gehandicapten",
+"Instelling voor verstandelijk gehandicapten",
+"Instelling voor visueel gehandicapten",
+"Hospice",
+"GGD",
+"Huisartsenpost",
+"Huisartsenpraktijk",
+"Zorghotel",
+"Geriatrische revalidatiezorg",
+"ziekenhuis"]
+
+url='https://www.zorgkaartnederland.nl/overzicht/organisatietypes'
+list1=[]
+res_s = requests.get(url)
+soup = BeautifulSoup(res_s.content,'html.parser')
+for items1 in soup.find_all('section', class_="content_section"):
+    for items2 in items1.find_all('ul', class_="list-group col-lg-6"):
+        for items3 in items2.find_all('li', class_="list-group-item"):
+            item=items3.find('a',href=True)
+            service_type=item.text
+            if service_type in service_list:             
+                link_item='https://www.zorgkaartnederland.nl'+item['href']
+                found = re.search('\((.+?)\)', items3.text.split(' ')[-1]).group(1)
+                pages_no = math.ceil(int(found)/20) if math.ceil(int(found)/20) < 9000 else 1000
+                for elm in range(1,pages_no+1):
+                    time.sleep(1)
+                    url_s=link_item +'/pagina{}'.format(elm)
+                    res_s = requests.get(url_s)
+                    soup_s = BeautifulSoup(res_s.content,'html.parser')
+                    for items4 in soup_s.find_all('li', class_="media"):
+                        lat_lon=items4['data-location']
+                        items5=items4.find('div',class_="media-body")
+                        service_naam=items5.find('h4',class_="media-heading title orange").get_text().replace("\n", "")
+                        item6=items5.find('a',href=True)
+                        Locatie=items5.find('span',class_="context").get_text()
+                        #print(service_type,service_naam,Locatie,lat_lon)
+                        service_info=service_type+";"+service_naam+";"+Locatie+";"+lat_lon
+                        list1.append(service_info)
+                        #item3=items2.find('span',class_="context").get_text().replace("\n", "").replace("    ", "") 
+                        #list1.append(item3)                                       
+#%%  for service provide
+                        
+fname=open("C:/Users/ATeklesadik/Rode Kruis/Data Team COVID-19 - [PRJ] Vulnerability and Capacity Analysis/RK capacity/P01_rk_branch_locations/data/GDC_content2.txt",'w')
+fname.write('"service_type";"service_naam";"Locatie";"lat-lon"'+'\n')
+for items in list1:
+        fname.write(items+'\n') 
+fname.close()                      
 
 
 
